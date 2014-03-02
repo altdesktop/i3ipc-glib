a9bd8e6ca0c10e209fb0c580cca2df8440ba5aa9
Tony Crisci <tony@dubstepdish.com>
2014-03-02 08:12:51 -0500

    Add top include file

---
+++ i3ipc/i3ipc-glib.h
@@ -0,0 +1,8 @@
+#ifndef __I3IPC_GLIB_H__
+#define __I3IPC_GLIB_H__
+
+#include <i3ipc-glib/i3ipc-enum-types.h>
+#include <i3ipc-glib/i3ipc-con.h>
+#include <i3ipc-glib/i3ipc-connection.h>
+
+#endif /* __I3IPC_GLIB_H__ */
